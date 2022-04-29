a753a80e9b240373faa1b95296039dbc0e76d4bd
miiike3 <mtramontano@wesleyan.edu>
2022-04-28 23:36:24 -0400

    Trying new ratesong

--- Functions/RateSong.js
+++ Functions/RateSong.js
@@ -8,21 +8,63 @@ const RateSong = ({ onAdd }) => {
 
   const [rating, setRating] = useState(5);
   const [song, setTitle] = useState("");
+  const [ratings, setRatings] = useState([]);
 
-  const userid = 1; 
-  const username = "mike";
+  const userid = 1;
+  const username = "mike"
+  // const userid = AsyncStorage.getItem('user_id');
+  // const username = AsyncStorage.getItem('username');
 
+  useEffect(() => {
+    refreshSongs();
+  }, []);
+
+  const refreshSongs = () => {
+    fetch("http://localhost:8000/api/songs/")
+      .then((res) => res.json())
+      .then((json) => setSongs(json))
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+    fetch("http://localhost:8000/api/ratings/")
+      .then((res) => res.json())
+      .then((json) => setRatings(json))
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    let item = {
-      'username' : userid,
-      'song' : song,
-      'rating' : rating};   
-    axios.post("http://localhost:8000/api/ratings/", item)
-    .then(alert("Rating Submitted"))
-    .catch((err) => alert(err));       
+    let lst = []
+    lst = ratings.filter(item => item.username == userid);
+    lst = lst.filter(item => item.song == song);
+    if (lst.length == 0) {
+      let item = {
+        'username' : userid,
+        'song' : song,
+        'rating' : rating};   
+      axios.post("http://localhost:8000/api/ratings/", item)
+      .then(alert("Rating Submitted"))
+      .catch((err) => alert(err));   
+    }
+    else {
+      for (let i in lst) {
+        axios.put(`http://localhost:8000/api/ratings/${i.rating_id}/`, item)
+        .then(alert("Rating Changes"))
+        .catch((err) => alert(err));
+    }    
+  }
   };
+
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
+  //   let item = {
+  //     'username' : userid,
+  //     'song' : song,
+  //     'rating' : rating};   
+  //   axios.post("http://localhost:8000/api/ratings/", item)
+  //   .then(alert("Rating Submitted"))
+  //   .catch((err) => alert(err));       
+  // };
 
   return(
     <View style={{ flex: 1, padding: 24}}>
